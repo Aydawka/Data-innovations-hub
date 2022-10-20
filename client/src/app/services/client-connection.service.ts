3d5072ceeb95ddc96d9d0489e4f78024043744f2
aydan gasimova <aydan.gasimova@und.edu>
2022-10-20 15:47:21 -0500

    deleted class

--- client/src/app/services/client-connection.service.ts
+++ client/src/app/services/client-connection.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {map, Observable, tap} from "rxjs";
-import {Files} from "../models/files";
+
 
 
 
