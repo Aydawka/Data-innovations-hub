b87971e597e2fb2e962521f93ea32a330f8f1df4
aydan gasimova <aydan.gasimova@und.edu>
2022-10-20 15:48:15 -0500

    updated reset, submit, and upload buttons

--- client/src/app/file-uploads/file-uploads.component.ts
+++ client/src/app/file-uploads/file-uploads.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ClientConnectionService} from "../services/client-connection.service";
 import {MessageService} from "primeng/api";
 import {catchError, Observable, of, throwError} from "rxjs";
-import {Files} from "../models/files";
 import {error} from "@angular/compiler-cli/src/transformers/util";
+import {HttpErrorResponse} from "@angular/common/http";
+import {Warnings} from "@angular/cli/lib/config/workspace-schema";
+import {NgForm} from "@angular/forms";
+
+interface AfterViewInit {
+}
 
 @Component({
   selector: 'app-file-uploads',
   templateUrl: './file-uploads.component.html',
   styleUrls: ['./file-uploads.component.css']
 })
-export class FileUploadsComponent{
+
+export class FileUploadsComponent implements AfterViewInit{
 
   public files: File[] = [];
   public message: string='';
@@ -18,8 +24,14 @@ export class FileUploadsComponent{
 
   constructor(
     private messageService: MessageService,
-    private readonly clientConnection:ClientConnectionService
+    private readonly clientConnection:ClientConnectionService,
   )  {}
+
+  @ViewChild("uploadForm")
+  protected form:NgForm|null=null;
+
+  @ViewChild("filesInput")
+  protected input: HTMLInputElement|null=null;
 
   onFileChange(event: Event){
     let target = event.target as HTMLInputElement;
@@ -27,8 +39,8 @@ export class FileUploadsComponent{
     console.log(event);
   }
 
+  //sending files to Flask
   public onSubmit(): void{
-
     if(!this.files.length)
       return;
 
@@ -37,8 +49,9 @@ export class FileUploadsComponent{
         catchError(
           (error) =>
           {
-            let e = error.error instanceof ErrorEvent ? error.error : error;
+            let e = error.error instanceof ErrorEvent ? error.error : this.getServerMessage(error);
             this.error = e.message;
+
             console.log(e);
             throw error;
           }
@@ -47,20 +60,42 @@ export class FileUploadsComponent{
       .subscribe(
         (message: string)=>
         {
+          this.reset();
           this.message=message;
-          this.error = '';
+
         }
       )
   }
 
-  public reset(){
-    let r=this.files=[];
-    let s=this.message='';
-    let a= this.error=''
-    return r||s||a
+  //display message if forbidden type of data is entered
+  public getServerMessage(error: HttpErrorResponse): string {
+      switch (error.status) {
+        case 404: {
+          return `Not Found: ${error.message}`;
+        }
+        case 403: {
+          return `Access Denied: ${error.message}`;
+        }
+        case 500: {
+          return `Internal Server Error: ${error.message}`;
+        }
+        default: {
+          return `Unknown Server Error: ${error.message}`;
+        }
+      }
+
+  }
+// to reset whole system they
+  public reset():void{
+    this.message='';
+    this.error='';
+
+    this.files=[];
+    this.form!.resetForm();
+    this.input!.value='';
   }
 
-
+//delete single files
  public delete(file:any){
    const index = this.files.indexOf(file, 0);
    this.files.splice(index, 1);
